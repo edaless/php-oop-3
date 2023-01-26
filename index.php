f7fda389b65223398a354f2e53bf38a131b5e330
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-01-26 22:23:08 +0100

    quattordicesima funzionante

--- index.php
+++ index.php
@@ -5,16 +5,19 @@ class Stipendio
 
     private $mensile;
     private $tredicesima;
+    private $quattordicesima;
 
 
 
     public function __construct(
         $mensile,
-        $tredicesima
+        $tredicesima,
+        $quattordicesima
     ) {
 
         $this->setMensile($mensile);
         $this->setTredicesima($tredicesima);
+        $this->setQuattordicesima($quattordicesima);
     }
 
 
@@ -47,14 +50,39 @@ class Stipendio
 
 
 
-    public function getStipendio()
+
+    public function getQuattordicesima()
     {
 
-        return ($this->getMensile() * 12) . "<br>";
+        return $this->quattordicesima;
+    }
+    public function setQuattordicesima($quattordicesima)
+    {
+
+        $this->quattordicesima = $quattordicesima;
+    }
+
+
+    // se c'è la quattordicesima non è importante come è settata la tredicesima
+    public function getStipendioAnnuo()
+    {
+        if ($this->quattordicesima) {
+
+            return ($this->getMensile() * 14) . "<br>";
+        } else if ($this->tredicesima) {
+
+            return ($this->getMensile() * 13) . "<br>";
+        } else {
+
+            return ($this->getMensile() * 12) . "<br>";
+        };
     }
 }
 
 
-$stipendio = new Stipendio(1000, false);
+$stipendio = new Stipendio(1000, false, true);
 
-echo $stipendio->getStipendio();
+echo $stipendio->getStipendioAnnuo();
+
+
+echo "<br><br><br>okkkk";
